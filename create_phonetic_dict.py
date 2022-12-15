4bc9a89fa67036fb9c43dffdc635b2cd1a8f5b1d
tango1556132 <120644996+tango1556132@users.noreply.github.com>
2022-12-15 14:57:21 +0200

    xsampa

--- create_phonetic_dict.py
+++ create_phonetic_dict.py
@@ -8,18 +8,112 @@ from russian_tagsets import converters
 from russian_g2p.Grapheme2Phoneme import Grapheme2Phoneme
 
 
+class Lexicon(object):
+
+    def __init__(self, phones):
+        """ phones: list of strings, each being a phone """
+        self.phone_set = set(self.make_position_independent(phones))
+
+    # XSAMPA phones are 1-letter each, so 2-letter below represent 2 separate phones.
+    CMU_to_XSAMPA_dict = {
+        "'"   : "'",
+        'AA'  : 'A',
+        'AE'  : '{',
+        'AH'  : 'V',  ##
+        'AO'  : 'O',  ##
+        'AW'  : 'aU',
+        'AY'  : 'aI',
+        'B'   : 'b',
+        'CH'  : 'tS',
+        'D'   : 'd',
+        'DH'  : 'D',
+        'EH'  : 'E',
+        'ER'  : '3',
+        'EY'  : 'eI',
+        'F'   : 'f',
+        'G'   : 'g',
+        'HH'  : 'h',
+        'IH'  : 'I',
+        'IY'  : 'i',
+        'JH'  : 'dZ',
+        'K'   : 'k',
+        'L'   : 'l',
+        'M'   : 'm',
+        'NG'  : 'N',
+        'N'   : 'n',
+        'OW'  : 'oU',
+        'OY'  : 'OI', ##
+        'P'   : 'p',
+        'R'   : 'r',
+        'SH'  : 'S',
+        'S'   : 's',
+        'TH'  : 'T',
+        'T'   : 't',
+        'UH'  : 'U',
+        'UW'  : 'u',
+        'V'   : 'v',
+        'W'   : 'w',
+        'Y'   : 'j',
+        'ZH'  : 'Z',
+        'Z'   : 'z',
+    }
+    CMU_to_XSAMPA_dict.update({'AX': '@'})
+    del CMU_to_XSAMPA_dict["'"]
+    XSAMPA_to_CMU_dict = { v: k for k,v in CMU_to_XSAMPA_dict.items() }  # FIXME: handle double-entries
+
+    @classmethod
+    def phones_cmu_to_xsampa_generic(cls, phones, lexicon_phones=None):
+        new_phones = []
+        for phone in phones:
+            stress = False
+            if phone.endswith('1'):
+                phone = phone[:-1]
+                stress = True
+            elif phone.endswith(('0', '2')):
+                phone = phone[:-1]
+            phone = cls.CMU_to_XSAMPA_dict[phone]
+            assert 1 <= len(phone) <= 2
+
+            new_phone = ("'" if stress else '') + phone
+            if (lexicon_phones is not None) and (new_phone in lexicon_phones):
+                # Add entire possibly-2-letter phone
+                new_phones.append(new_phone)
+            else:
+                # Add each individual 1-letter phone
+                for match in re.finditer(r"('?).", new_phone):
+                    new_phones.append(match.group(0))
+
+        return new_phones
+
+    def phones_cmu_to_xsampa(self, phones):
+        return self.phones_cmu_to_xsampa_generic(phones, self.phone_set)
+
+    @classmethod
+    def make_position_dependent(cls, phones):
+        if len(phones) == 0: return []
+        elif len(phones) == 1: return [phones[0]+'_S']
+        else: return [phones[0]+'_B'] + [phone+'_I' for phone in phones[1:-1]] + [phones[-1]+'_E']
+
+    @classmethod
+    def make_position_independent(cls, phones):
+        return [re.sub(r'_[SBIE]', '', phone) for phone in phones]
+
+
 def transcribe_words(source_words_list):
     bad_words = []
     transcriptions = []
     
     trans = Grapheme2Phoneme()
+    lexicon = Lexicon()
+
     for word_unformated in source_words_list:
         word = word_unformated.replace("\u0301", "+")
         word = word.replace("о́", "o+")
         
         transcription = [word_unformated]
         for phone in trans.word_to_phonemes(word):
-            transcription.append(phone)
+            xsampa_phone = lexicon.phones_cmu_to_xsampa(phone)
+            transcription.append(xsampa_phone)
         print(transcription)
         transcriptions.append(transcription)
     return transcriptions, bad_words
@@ -118,9 +212,23 @@ def main():
     transcriptions, bad_words = transcribe_words(sorted(source_words))
     with codecs.open(dst_name, mode='w', encoding='utf-8', errors='ignore') as fp:
         for cur in transcriptions:
-            for sound in cur:
+            # write accented word
+            for index, sound in enumerate(cur):
                 fp.write('{0} '.format(sound))
             fp.write("\n")
+
+            # write un-accented word
+            word = cur[0]
+            word = word.replace("\u0301", "+")
+            # word = word.replace("о́", "o+")
+            fp.write('{0} '.format(word))
+            for index, sound in enumerate(cur):
+                # skip word
+                if index == 0:
+                    continue 
+                fp.write('{0} '.format(sound))
+            fp.write("\n")
+
 
     if len(bad_words) > 0:
         with codecs.open(bad_name, mode='w', encoding='utf-8', errors='ignore') as fp:
